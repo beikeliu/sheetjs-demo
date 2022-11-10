ad4b3faf0e1fde301ccba9851c164c8aedc1dce4
v_beileiliu <v_beileiliu@tencent.com>
2022-11-10 16:34:54 +0800

    xlsx导入导出练习

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import { read, utils, writeFileXLSX } from 'xlsx'
+import { useState } from 'react';
+import './ktable.css';
+const KTable= ({ rowKey, data, option }) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <table className="k-table">
+        <thead>
+          <tr>
+            {option.map((op) => (
+              <th key={op.title}>{op.title}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((dt, index) => (
+            <tr key={dt[rowKey]}>
+              {option.map((op) => (
+                <td key={op.title}>
+                  {op.render
+                    ? op.render(op.in ? dt[op.in] : null, dt, index)
+                    : op.in
+                    ? dt[op.in]
+                    : null}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
   );
+};
+
+const App = () => {
+  const [data, setData] = useState([]);
+  const [option, setOption] = useState([]);
+  const onImport = async (e) => {
+    const file = e.target.files[0];
+    const data = await file.arrayBuffer();
+    const wb = read(data);
+    const ws = wb.Sheets[wb.SheetNames[0]];
+    const json = utils.sheet_to_json(ws);
+    setData(json);
+    setOption(Object.keys(json[0]).map((k) => ({
+      title: k,
+      in: k,
+    })))
+  }
+  const onExport = () => {
+    const ws = utils.json_to_sheet(data);
+    const wb = utils.book_new();
+    utils.book_append_sheet(wb, ws);
+    writeFileXLSX(wb, "test.xlsx");
+  }
+  return (
+    <>
+      <input type='file' onChange={onImport} onClick={(e) => e.target.value = null} />
+      <button onClick={onExport}>导出</button>
+      <KTable rowKey="姓名" data={data} option={option}/>
+    </>
+  )
 }
 
 export default App;
